077fe39bfb98fe4ee0029d7c81836aa58af2a20d
Sophia <sophia852686@gmail.com>
2022-03-08 11:28:51 +1100

    motify layout

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ function App() {
   //create a useState to disply data on the screen
   const [randomUser, setRandomUser] = useState([]);
   const [searchGender, setSearchGender] = useState('');
+  const [searchName, setSearchName] = useState('')
 
   //Make API request
   useEffect(() => {
-    Axios.get("https://randomuser.me/api/?results=5")
+    Axios.get("https://randomuser.me/api/?results=50")
       .then((respone) => {
         setRandomUser(respone.data.results);
         console.log(respone.data.results)
@@ -21,7 +22,7 @@ function App() {
 
   //Create filleredGender
   const filleredGender = randomUser.filter((user) => {
-    // return user.gender.toLowerCase().indexOf(searchGender) > -1
+    //return user.name.toLowerCase().indexOf(searchName) > -1
     if (searchGender === "male") {
       return user.gender.toLowerCase() === "male"
     } else if (searchGender === "female") {
@@ -32,26 +33,38 @@ function App() {
 
   });
 
+
+
+
   return (
     <div className="App">
       <h1>Random User App</h1>
-      <div >
+      <div  >
         <input type="text" placeholder="Search gender..." value={searchGender}
           onChange={(event) => {
             setSearchGender(event.target.value);
             console.log(event.target.value)
           }} />
+
+
+
       </div>
-      <div >
+
+
+
+      <div className="users">
         {filleredGender.map((user, key) => {
           return (
             <Users
               key={key} //unqiue key
               first={user.name.first}
               last={user.name.last}
+              age={user.dob.age}
+              nat={user.nat}
               email={user.email}
-              picture={user.picture.large} s
+              picture={user.picture.large}
               gender={user.gender}
+              phone={user.phone}
             />
           );
         })}
